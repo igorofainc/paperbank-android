20adf06b0f5628d3a992e266e73314f5e1cd45d9
LZRS <lazloti@gmail.com>
2017-03-01 00:54:06 +0300

    added get started page

--- app/src/main/java/com/igorofa/paperbank/paperbank/activities/PaperBankGetStartedActivity.java
+++ app/src/main/java/com/igorofa/paperbank/paperbank/activities/PaperBankGetStartedActivity.java
@@ -1,8 +1,10 @@
 package com.igorofa.paperbank.paperbank.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.igorofa.paperbank.paperbank.R;
 
@@ -17,5 +19,14 @@ public class PaperBankGetStartedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_get_started_paper_bank);
+
+        findViewById(R.id.get_started_text_view)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(PaperBankGetStartedActivity.this, PaperBankMainActivity.class));
+                        finish();
+                    }
+                });
     }
 }
--- app/src/main/java/com/igorofa/paperbank/paperbank/activities/PaperBankMainActivity.java
+++ app/src/main/java/com/igorofa/paperbank/paperbank/activities/PaperBankMainActivity.java
@@ -1,9 +1,9 @@
 package com.igorofa.paperbank.paperbank.activities;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -45,8 +45,7 @@ public class PaperBankMainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(PaperBankMainActivity.this, PaperBankFilesActivity.class));
             }
         });
     }
