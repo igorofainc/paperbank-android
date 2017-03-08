3c2314cbd88c7dd64efddb38826d241cc660c9da
LZRS <lazloti@gmail.com>
2017-03-08 22:59:08 +0300

    working with rx

--- app/src/main/java/com/igorofa/paperbank/paperbank/activities/PaperBankGetStartedActivity.java
+++ app/src/main/java/com/igorofa/paperbank/paperbank/activities/PaperBankGetStartedActivity.java
@@ -8,11 +8,21 @@ import android.view.View;
 
 import com.igorofa.paperbank.paperbank.R;
 
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.subjects.PublishSubject;
+
 /**
  * Created by x4b1d on 21/02/17.
  */
 
 public class PaperBankGetStartedActivity extends AppCompatActivity {
+
+    /*
+     * rx subject that receives only the subsequent items after subscription
+     * subscribers to click events will only receive subsequent clicks after subscription
+     */
+    PublishSubject<View> getStartedTextObservable = PublishSubject.create();
+    CompositeDisposable mCompositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,12 +31,29 @@ public class PaperBankGetStartedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_get_started_paper_bank);
 
         findViewById(R.id.get_started_text_view)
-                .setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(new Intent(PaperBankGetStartedActivity.this, PaperBankMainActivity.class));
-                        finish();
-                    }
-                });
+                .setOnClickListener(v -> getStartedTextObservable.onNext(v));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        bind();
+    }
+
+    void bind(){
+        mCompositeDisposable.add(getStartedTextObservable.subscribe(view -> {
+            startActivity(new Intent(view.getContext(), PaperBankMainActivity.class));
+            finish();
+        }));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbind();
+    }
+
+    void unbind(){
+        mCompositeDisposable.clear();
     }
 }
--- app/src/main/java/com/igorofa/paperbank/paperbank/activities/PaperBankMainActivity.java
+++ app/src/main/java/com/igorofa/paperbank/paperbank/activities/PaperBankMainActivity.java
@@ -1,13 +1,14 @@
 package com.igorofa.paperbank.paperbank.activities;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,6 +21,7 @@ public class PaperBankMainActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerView;
     PaperAdapter mPaperAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,19 +43,37 @@ public class PaperBankMainActivity extends AppCompatActivity {
         mPaperAdapter = new PaperAdapter(this);
         mRecyclerView.setAdapter(mPaperAdapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(PaperBankMainActivity.this, PaperBankFilesActivity.class));
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(PaperBankMainActivity.this, PaperBankFilesActivity.class));
+//            }
+//        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_paper_bank_main, menu);
+
+        SearchView toolbarSearchView = (SearchView) findViewById(R.id.toolbar_search_view);
+
+//        int search_plate_id =  toolbarSearchView.getContext().getResources().getIdentifier("android:id/search_plate", null, null);
+//        Log.d(PaperBankMainActivity.class.getSimpleName(), "" + search_plate_id);
+        View search_plate = toolbarSearchView.findViewById(R.id.search_plate);
+        if (search_plate != null){
+            Log.d(PaperBankMainActivity.class.getSimpleName(), "Is search_plate really not null");
+            search_plate.setBackgroundColor(Color.DKGRAY);
+
+            int searchTextId = search_plate.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+            TextView searchText = (TextView) search_plate.findViewById(searchTextId);
+            if (searchText!=null) {
+                searchText.setTextColor(Color.WHITE);
+                searchText.setHintTextColor(Color.WHITE);
+            }
+        }
+
         return true;
     }
 
