ab488048eef28517e548f93e1941c26d400bba0b
LZRS <lazloti@gmail.com>
2017-03-20 11:34:10 +0300

    disabled some stuff

--- app/src/main/java/com/igorofa/paperbank/paperbank/activities/PaperBankMainActivity.java
+++ app/src/main/java/com/igorofa/paperbank/paperbank/activities/PaperBankMainActivity.java
@@ -1,8 +1,14 @@
 package com.igorofa.paperbank.paperbank.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +24,7 @@ import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.subjects.PublishSubject;
 
 public class PaperBankMainActivity extends AbstractToolbarActivity {
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 0;
     private PBMainActivityViewModel mMainActivityViewModel;
 
     // Observer that publishes click events on the fab button
@@ -38,6 +45,8 @@ public class PaperBankMainActivity extends AbstractToolbarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        showRequestPermission();
+
         mMainActivityViewModel = ((PaperBankApp)getApplicationContext()).getMainActivityViewModel();
 
         mPaperAdapter = new PaperAdapter(this, mMainActivityViewModel,mMainActivityViewModel.getPapers());
@@ -58,6 +67,12 @@ public class PaperBankMainActivity extends AbstractToolbarActivity {
 //        uploadFileFloatingActionButton = (FloatingActionButton) findViewById(R.id.upload_file_floating_button);
         recentFilesFloatingActionButton = (FloatingActionButtonWithHintText) findViewById(R.id.recent_files_floating_button);
         recentFilesFloatingActionButton.setImageSrcRes(R.drawable.ic_recent_files); // set image src manually...setting through xml had issues
+
+        mRecyclerView.setOnTouchListener((v, event) -> {
+
+            mainFloatingActionButton.setStateOpen(false);
+            return false;
+        });
     }
 
     @Override
@@ -132,4 +147,67 @@ public class PaperBankMainActivity extends AbstractToolbarActivity {
 //
 //        return super.onOptionsItemSelected(item);
 //    }
+
+
+    private void showRequestPermission(){
+        Log.d(PaperBankMainActivity.class.getSimpleName(), "called");
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            Log.d(PaperBankMainActivity.class.getSimpleName(), "no permission");
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "App needs storage permission to download papers, You can still change it in Apps settings", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 }
